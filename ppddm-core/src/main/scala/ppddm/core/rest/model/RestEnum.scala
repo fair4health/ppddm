47e1235cd43d71f8f8a39ce2e3678261662dd108
A. Anil SINACI <anil@srdc.com.tr>
2020-08-28 11:16:13 +0300

    feat: read agent(datasource) definitions from a configuration file; improve JSON parsing. fixes #20

--- ppddm-core/src/main/scala/ppddm/core/rest/model/RestEnum.scala
+++ ppddm-core/src/main/scala/ppddm/core/rest/model/RestEnum.scala
@@ -24,6 +24,18 @@ object DataSourceSelectionStatus extends Enumeration {
   val DISCARDED = "discarded"
 }
 
+/**
+ * ExecutionState is being used by Dataset and DatasetSource classes of the model.
+ *
+ * Dataset: When a Dataset is created, it will invoke Agents to prepare their data. The ExecutionState of the Dataset
+ * will be QUERYING at this point. Once all Agents prepare their data, th ExecutionState of the Dataset will be
+ * IN_PROGRESS since the user now will select/discard the Dataset. After this selection, the ExecutionState will be FINAL.
+ *
+ * DatasetSource: When an Agent is invoked to prepare its data, the associated DatasetSource's ExecutionState will be
+ * QUERYING. When the data is ready at the Agent, then the ExecutionState will be FINAL. Hence, DatasetSource uses only
+ * two of the available ExecutionState enumeration values.
+ *
+ */
 object ExecutionState extends Enumeration {
   type ExecutionState = String
   val QUERYING = "querying"
