2e21ae69c45131972594bb7254e0b42ce5609f28
Alper Teoman <teooalp@hotmail.com>
2020-09-22 18:12:44 +0300

    :sparkles: feat(fhir-query): Return all queried resources. Closes: #60

--- ppddm-core/src/main/scala/ppddm/core/fhir/FHIRQuery.scala
+++ ppddm-core/src/main/scala/ppddm/core/fhir/FHIRQuery.scala
@@ -1,5 +1,6 @@
 package ppddm.core.fhir
 
+import org.json4s.JsonAST.{JString, JValue}
 import org.json4s.{JArray, JInt, JObject}
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -34,12 +35,51 @@ abstract class FHIRQuery(fhirPath: Option[String]) extends Serializable {
     getPageQuery(count, pageIndex).getResources(client)
   }
 
-  def getResources(client: FHIRClient): Future[Seq[JObject]] = {
-    // TODO: This query only retrieves the 1st page (default 20).
-    // TODO: All resources should be returned by this function.
-    execute(client) map { bundle =>
-      (bundle \ "entry").asInstanceOf[JArray].arr.map { entry =>
-        (entry \ "resource").asInstanceOf[JObject]
+  /**
+   * Returns the resulting resources for the query
+   *
+   * @param client Fhir Client
+   * @param all Boolean value to understand whether it wants all the resources to be retrieved or the resources on the first page
+   * @return
+   */
+  def getResources(client: FHIRClient, all: Boolean = false): Future[Seq[JObject]] = {
+    if (all) {
+      getAllResources(client, Seq.empty[JObject], 1)
+    } else {
+      execute(client) map { bundle =>
+        (bundle \ "entry").asInstanceOf[JArray].arr.map { entry =>
+          (entry \ "resource").asInstanceOf[JObject]
+        }
+      }
+    }
+  }
+
+  /**
+   * Returns all the resources recursively
+   *
+   * @param client Fhir Client
+   * @param resources Seq of JObjects - provide Seq.empty[JObject] as initial value
+   * @param pageIndex Page index - provide 1 as initial value
+   * @return
+   */
+  private def getAllResources(client: FHIRClient, resources: Seq[JObject], pageIndex: Int): Future[Seq[JObject]] = {
+    var currResources: Seq[JObject] = resources
+    // Create query with page size 20 and given page index
+    getPageQuery(20, pageIndex).execute(client) flatMap { bundle =>
+      // Get the bundle.link array
+      val bundleLink = (bundle \ "link").asInstanceOf[JArray].arr
+      // Search for 'next' relation in the bundle.link object to detect whether the query response has next page
+      val hasNext: Option[JValue] = bundleLink.find(item => (item \ "relation").asInstanceOf[JString].s.equals("next"))
+      // Expand the resource list with resources on the current page index
+      (bundle \ "entry").asInstanceOf[JArray].arr.foreach { entry =>
+        currResources = currResources :+ (entry \ "resource").asInstanceOf[JObject]
+      }
+      if (hasNext.nonEmpty) { // If the response has the next page, increment the page index and then call getAllResources function again
+        getAllResources(client, currResources, pageIndex + 1)
+      } else { // If not, return current resource list
+        Future {
+          currResources
+        }
       }
     }
   }
