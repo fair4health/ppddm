8062f915205d2688ef2c2c0b1c0841966bf4731e
A. Anil Sinaci <anil@srdc.com.tr>
2021-06-01 14:54:40 +0300

    :memo: docs: Fix an inline comment.

--- ppddm-agent/src/main/scala/ppddm/agent/config/AgentConfig.scala
+++ ppddm-agent/src/main/scala/ppddm/agent/config/AgentConfig.scala
@@ -9,7 +9,7 @@ object AgentConfig extends AppConfig {
   /** Application name */
   override lazy val appName: String = Try(config.getString("app.name")).getOrElse("FAIR4Health PPDDM-Agent")
 
-  /** Base URI for mantIQ Services to be served from */
+  /** Base URI to be served from */
   override lazy val baseUri: String = Try(config.getString("server.base-uri")).getOrElse("agent")
 
   /** FHIR endpoint */
