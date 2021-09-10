14a5c36f35414cfdd96cbeb38d6c84841b8313fa
A. Anil Sinaci <anil@srdc.com.tr>
2021-09-10 18:21:15 +0300

    :sparkles: feat(exceptions): Return the exception stack trace in HTTP response in case of error.

--- ppddm-manager/src/main/scala/ppddm/manager/gateway/api/directive/ManagerExceptionHandler.scala
+++ ppddm-manager/src/main/scala/ppddm/manager/gateway/api/directive/ManagerExceptionHandler.scala
@@ -7,6 +7,8 @@ import com.typesafe.scalalogging.Logger
 import ppddm.core.exception.{AuthException, DBException}
 import ppddm.manager.exception.{AgentCommunicationException, DataIntegrityException}
 
+import java.io.{PrintWriter, StringWriter}
+
 trait ManagerExceptionHandler {
   private val logger: Logger = Logger(this.getClass)
 
@@ -14,19 +16,25 @@ trait ManagerExceptionHandler {
   val ppddmExceptionHandler: ExceptionHandler = ExceptionHandler {
     case e: AuthException =>
       logger.error(s"AuthException: ${e.getMessage}", e)
-      complete(StatusCodes.Unauthorized -> s"Not Authorized. ${e.getMessage}")
+      complete(StatusCodes.Unauthorized -> s"Not Authorized. ${e.getMessage}.\n ${stackTraceToString(e)}")
     case e: DBException =>
       logger.error(s"DBException: ${e.getMessage}", e)
-      complete(StatusCodes.InternalServerError -> s"Error with the MongoDB Database. ${e.getMessage}")
+      complete(StatusCodes.InternalServerError -> s"Error with the MongoDB Database. ${e.getMessage}.\n ${stackTraceToString(e)}")
     case e: DataIntegrityException =>
       logger.error(s"DataIntegrityException: ${e.getMessage}", e)
-      complete(StatusCodes.InternalServerError -> s"Data integrity error. ${e.getMessage}")
+      complete(StatusCodes.InternalServerError -> s"Data integrity error. ${e.getMessage}.\n ${stackTraceToString(e)}")
     case e: AgentCommunicationException =>
       val msg = s"AgentCommunicationException while connecting to Agent on URI: ${e.url} whose name:${e.name}: ${e.getMessage}"
       logger.error(msg, e)
       complete(StatusCodes.InternalServerError -> msg)
     case e: Exception =>
       logger.error("Unknown Exception", e)
-      complete(StatusCodes.InternalServerError -> s"UNKNOWN EXCEPTION: ${e.getMessage}")
+      complete(StatusCodes.InternalServerError -> s"UNKNOWN EXCEPTION: ${e.getMessage}.\n ${stackTraceToString(e)}")
+  }
+
+  private def stackTraceToString(e: Exception): String = {
+    val sw = new StringWriter
+    e.printStackTrace(new PrintWriter(sw))
+    sw.toString
   }
 }
